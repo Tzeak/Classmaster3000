621e44eb989803ed90ef2db266bcbba92ad52146
Kyle Alwyn <kalwyn09@gmail.com>
2014-11-04 16:38:17 -0800

    inital operational system

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -69,6 +69,15 @@ angular.module('classmasterApp', ['ngAnimate','ngRoute'])
       $rootScope.winterengr = '';
       $scope.bc = false;
       $scope.ab = false;
+      $scope.chem = false;
+      $scope.coen = false;
+      $scope.fallcore = 0;
+      $scope.wintercore = 0;
+      $scope.fallco= true;
+      $scope.winterco = true;
+      $scope.fallsci = true;
+      $scope.wintersci = true;
+      $scope.falleng = true;
 
       $scope.click = function(){
         $timeout(function() {
@@ -118,10 +127,108 @@ angular.module('classmasterApp', ['ngAnimate','ngRoute'])
                 $rootScope.springmath = 'MATH 13';
               }
             }
+
+            if(value === 'Computer Science A' && key === true && $scope.chem === true){
+              $scope.coen = true;
+              if($rootScope.fallcoen === 'CORE'){
+                $scope.fallcore--;
+              }
+              if($rootScope.winterscience === 'CORE'){
+                $scope.wintercore--;
+              }
+              $rootScope.fallcoen = 'COEN 12';
+              $rootScope.wintercoen = 'COEN 70';
+              $rootScope.springcoen = 'COEN 20';
+            }
+            else if(value === 'Computer Science A' && key === true && $scope.chem === false){
+              $scope.coen = true;
+              $rootScope.fallcoen = 'CORE';
+              $scope.fallco = false;
+              $scope.fallcoencore = true;
+              $scope.fallcore++;
+              $rootScope.wintercoen = 'CORE';
+              $scope.winterco = false;
+              $scope.wintercoencore = true;
+              $scope.wintercore++;
+              $rootScope.springcoen = 'COEN 12';
+            }
+            else if(value === 'Computer Science A' && key === false){
+              $scope.coen = false;
+              if($rootScope.fallcoen === 'CORE'){
+                $scope.fallcore--;
+              }
+              if($rootScope.wintercore === 'CORE'){
+                $scope.wintercore--;
+              }
+              $rootScope.fallcoen = 'COEN 10';
+              $rootScope.wintercoen = 'COEN 11';
+              $rootScope.springcoen = 'COEN 12';
+              $scope.fallco = true;
+              $scope.fallcoencore = false;
+              $scope.winterco = true;
+              $scope.wintercoencore = false;
+            }
+
+            if(value === 'Chemistry' && key === true){
+              $scope.chem = true;
+              $rootScope.fallscience = 'CORE';
+              $scope.fallsci = false;
+              $scope.fallscicore = true;
+              $scope.fallcore++;
+            }
+            else if(value === 'Chemistry' && key === false){
+              $scope.chem = false;
+              if($scope.fallscience === 'CORE'){
+                $scope.fallcore--;
+              }
+              $rootScope.fallscience = 'CHEM 11';   
+              $scope.fallsci = true;
+              $scope.fallscicore = false;      
+            }
+
+            if(value === 'Physics C: Mechanics' && key === true){
+              if($rootScope.wintercoen === 'CORE'){
+                $scope.wintercore--;
+                $rootScope.fallcoen = 'COEN 12';
+                $rootScope.wintercoen = 'COEN 70';
+                $rootScope.springcoen = 'COEN 20';
+                $rootScope.winterscience = 'CORE';
+                $scope.wintersci = false;
+                $scope.winterscicore = true; 
+                $scope.wintercore++;
+              }
+              else{
+                $rootScope.winterscience = 'CORE';
+                $scope.wintercore++;
+                $scope.wintersci = false;
+                $scope.winterscicore = true; 
+              }
+            }
+            else if(value === 'Physics C: Mechanics' && key === false){
+              if($scope.winterscience === 'CORE'){
+                $scope.wintercore--;
+              }
+              $rootScope.winterscience = 'PHYS 31';
+              $scope.wintersci = true;
+              $scope.winterscicore = false; 
+            }
+
+            if($scope.wintercore > $scope.fallcore){
+              $rootScope.winterengr = 'ENGR 1';
+              $rootScope.fallengr = '';
+              $scope.falleng = false;
+              $scope.wintereng = true;
+            }
+            else if($scope.wintercore <= $scope.fallcore){
+              $rootScope.winterengr = '';
+              $rootScope.fallengr = 'ENGR 1';
+              $scope.falleng = true;
+              $scope.wintereng = false;
+            }
           }); //end for each
         }, 0); //end timeout
       }; //end click
     })
 
-.controller('ScheduleCtrl', function($rootScope) {
+.controller('ScheduleCtrl', function() {
   });

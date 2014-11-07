2d994e2bfe5e7f801f7ba6832bbd261a81094710
Kyle Alwyn <kalwyn09@gmail.com>
2014-11-07 11:18:29 -0800

    Update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,30 @@
 'use strict';
 
 angular.module('classmasterApp', ['ngAnimate','ngRoute'])
+  .constant('vars', {
+    ctw1: 'CTW 1',
+    ctw2: 'CTW 2',
+    math11: 'MATH 11',
+    math12: 'MATH 12',
+    math13: 'MATH 13',
+    math14: 'MATH 14',
+    math53: 'MATH 53',
+    amth106: 'AMTH 106',
+    amth108: 'AMTH 108',
+    coen10: 'COEN 10',
+    coen11: 'COEN 11',
+    coen12: 'COEN 12',
+    coen19: 'COEN 19',
+    coen20: 'COEN 20',
+    coen21: 'COEN 21',
+    candi1: 'C & I 1',
+    candi2: 'C & I 2',
+    chem11: 'CHEM 11',
+    phys31: 'PHYS 31',
+    phys32: 'PHYS 32',
+    engr1: 'ENGR 1',
+    core: 'University Core',
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -21,7 +45,7 @@ angular.module('classmasterApp', ['ngAnimate','ngRoute'])
   })
   .controller('MainCtrl', function() {
   })
-  .controller('AboutCtrl', function ($timeout,$scope,$rootScope) {
+  .controller('AboutCtrl', function ($window,$timeout,$scope,$rootScope,vars) {
     $scope.apselects = {};
     $scope.apclasses = [
         {
@@ -37,16 +61,13 @@ angular.module('classmasterApp', ['ngAnimate','ngRoute'])
           score:3
         },
         {
-          name:'Physics B',
-        },
-        {
           name:'Physics C: Mechanics',
           score:4
         },
-        {
-          name:'Physics C: Electricity & Magnetism',
-          score:4
-        },
+        // {
+        //   name:'Physics C: Electricity & Magnetism',
+        //   score:4
+        // },
         {
           name:'Chemistry',
           score:3
@@ -56,178 +77,81 @@ angular.module('classmasterApp', ['ngAnimate','ngRoute'])
           score:4
         }
       ];
-      $rootScope.fallmath = 'MATH 11';
-      $rootScope.wintermath = 'MATH 12';
-      $rootScope.springmath = 'MATH 13';
-      $rootScope.fallcoen = 'COEN 10';
-      $rootScope.wintercoen = 'COEN 11';
-      $rootScope.springcoen = 'COEN 12';
-      $rootScope.fallscience = 'CHEM 11';
-      $rootScope.winterscience = 'PHYS 31';
-      $rootScope.springscience = 'PHYS 32';
-      $rootScope.fallengr = 'ENGR 1';
-      $rootScope.winterengr = '';
-      $scope.bc = false;
-      $scope.ab = false;
-      $scope.chem = false;
-      $scope.coen = false;
-      $scope.fallcore = 0;
-      $scope.wintercore = 0;
-      $scope.fallco= true;
-      $scope.winterco = true;
-      $scope.fallsci = true;
-      $scope.wintersci = true;
-      $scope.falleng = true;
+      $scope.fallmath = vars.math11;
+      $scope.wintermath = vars.math12;
+      $scope.springmath = vars.math13;
+      $scope.fallcoen = vars.coen10;
+      $scope.wintercoen = vars.coen11;
+      $scope.springcoen = vars.coen12;
+      $scope.fallscience = vars.chem11;
+      $scope.winterscience = vars.phys31;
+      $scope.springscience = vars.phys32;
+      $scope.fallengr = vars.engr1;
+      $scope.isError = false;
+      $scope.errorCount = 0;
 
-      $scope.click = function(){
+      $scope.$watch('errorCount', function(){
+        $timeout(function(){
+          if($scope.errorCount === 0){
+          delete $scope.errorMessage;
+        }
+      },200);
+      });
+
+      function errorCheck(score){
+        if(score > 5 || score < 1){
+          $scope.errorCount++;
+          $scope.isError = true;
+          $scope.errorMessage = 'Enter a number between 1 and 5';
+        }
+      }
+
+      $scope.change = function(){
         $timeout(function() {
-          angular.forEach($scope.apselects, function(key,value){
-            
-            //if AB is checked
-            if(value === 'Calculus AB' && key === true){
-              $scope.ab = true;
-              if($scope.bc === false){
-                $rootScope.fallmath = 'MATH 12';
-                $rootScope.wintermath = 'MATH 13';
-                $rootScope.springmath = 'MATH 14';
-              }
+          angular.forEach($scope.apselects, function(score,apTest){
+            if(score !== null){
+              errorCheck(score);
             }
-
-            //if AB is not checked
-            else if(value === 'Calculus AB'){
-              $scope.ab = false;
-              //check if BC is checked for no overwrite
-              if($scope.bc === false){
-                $rootScope.fallmath = 'MATH 11';
-                $rootScope.wintermath = 'MATH 12';
-                $rootScope.springmath = 'MATH 13';
+            //if AB is checked
+            if(apTest === 'Calculus AB'){
+              if(score === 3){
+                $scope.calcAB3 = true;
+                $scope.calcAB45 = false;
+              }
+              else if(score > 3 && score <= 5){
+                $scope.calcAB45 = true;
+                $scope.calcAB3 = false;
+              }
+              else{
+                $scope.calcAB45 = false;
+                $scope.calcAB3 = false;
               }
             }
 
             //if BC is checked
-            if(value === 'Calculus BC' && key === true){
-              $scope.bc = true;
-              $rootScope.fallmath = 'MATH 13';
-              $rootScope.wintermath = 'MATH 14';
-              $rootScope.springmath = 'AMTH 106';
+            if(apTest === 'Calculus BC'){
             }
 
-            //if BC is not checked
-            else if(value === 'Calculus BC'){
-              $scope.bc = false;
-              //check if AB is still checked
-              if($scope.ab === true){ 
-                $rootScope.fallmath = 'MATH 12';
-                $scope.wintermath = 'MATH 13';
-                $scope.springmath = 'MATH 14';
-              }
-              else{
-                $rootScope.fallmath = 'MATH 11';
-                $rootScope.wintermath = 'MATH 12';
-                $rootScope.springmath = 'MATH 13';
-              }
+            if(apTest === 'Computer Science A'){
+
+            }
+            else if(apTest === 'Computer Science A'){
+            
             }
 
-            if(value === 'Computer Science A' && key === true && $scope.chem === true){
-              $scope.coen = true;
-              if($rootScope.fallcoen === 'CORE'){
-                $scope.fallcore--;
-              }
-              if($rootScope.winterscience === 'CORE'){
-                $scope.wintercore--;
-              }
-              $rootScope.fallcoen = 'COEN 12';
-              $rootScope.wintercoen = 'COEN 70';
-              $rootScope.springcoen = 'COEN 20';
-            }
-            else if(value === 'Computer Science A' && key === true && $scope.chem === false){
-              $scope.coen = true;
-              $rootScope.fallcoen = 'CORE';
-              $scope.fallco = false;
-              $scope.fallcoencore = true;
-              $scope.fallcore++;
-              $rootScope.wintercoen = 'CORE';
-              $scope.winterco = false;
-              $scope.wintercoencore = true;
-              $scope.wintercore++;
-              $rootScope.springcoen = 'COEN 12';
-            }
-            else if(value === 'Computer Science A' && key === false){
-              $scope.coen = false;
-              if($rootScope.fallcoen === 'CORE'){
-                $scope.fallcore--;
-              }
-              if($rootScope.wintercore === 'CORE'){
-                $scope.wintercore--;
-              }
-              $rootScope.fallcoen = 'COEN 10';
-              $rootScope.wintercoen = 'COEN 11';
-              $rootScope.springcoen = 'COEN 12';
-              $scope.fallco = true;
-              $scope.fallcoencore = false;
-              $scope.winterco = true;
-              $scope.wintercoencore = false;
+            if(apTest === 'Chemistry'){
             }
 
-            if(value === 'Chemistry' && key === true){
-              $scope.chem = true;
-              $rootScope.fallscience = 'CORE';
-              $scope.fallsci = false;
-              $scope.fallscicore = true;
-              $scope.fallcore++;
-            }
-            else if(value === 'Chemistry' && key === false){
-              $scope.chem = false;
-              if($scope.fallscience === 'CORE'){
-                $scope.fallcore--;
-              }
-              $rootScope.fallscience = 'CHEM 11';   
-              $scope.fallsci = true;
-              $scope.fallscicore = false;      
-            }
-
-            if(value === 'Physics C: Mechanics' && key === true){
-              if($rootScope.wintercoen === 'CORE'){
-                $scope.wintercore--;
-                $rootScope.fallcoen = 'COEN 12';
-                $rootScope.wintercoen = 'COEN 70';
-                $rootScope.springcoen = 'COEN 20';
-                $rootScope.winterscience = 'CORE';
-                $scope.wintersci = false;
-                $scope.winterscicore = true; 
-                $scope.wintercore++;
-              }
-              else{
-                $rootScope.winterscience = 'CORE';
-                $scope.wintercore++;
-                $scope.wintersci = false;
-                $scope.winterscicore = true; 
-              }
-            }
-            else if(value === 'Physics C: Mechanics' && key === false){
-              if($scope.winterscience === 'CORE'){
-                $scope.wintercore--;
-              }
-              $rootScope.winterscience = 'PHYS 31';
-              $scope.wintersci = true;
-              $scope.winterscicore = false; 
-            }
-
-            if($scope.wintercore > $scope.fallcore){
-              $rootScope.winterengr = 'ENGR 1';
-              $rootScope.fallengr = '';
-              $scope.falleng = false;
-              $scope.wintereng = true;
-            }
-            else if($scope.wintercore <= $scope.fallcore){
-              $rootScope.winterengr = '';
-              $rootScope.fallengr = 'ENGR 1';
-              $scope.falleng = true;
-              $scope.wintereng = false;
+            if(apTest === 'Physics C: Mechanics'){
             }
           }); //end for each
-        }, 0); //end timeout
+        }, 10); //end timeout
+        $scope.errorCount = 0;
       }; //end click
+
+      $scope.print = function(){
+        $window.print();
+      };
     })
 
 .controller('ScheduleCtrl', function() {
